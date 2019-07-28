d3ffc17dbcd72230441a4d38fe61d0d3d1d0999e
gohara_hiroaki <hiroaki.gohara@gmail.com>
2019-07-28 21:08:19 +0900

    LastIndexOf

--- data/data.go
+++ data/data.go
@@ -15,3 +15,17 @@ func GetEqualIndexOf(from []interface{}, val interface{}) int {
 	}
 	return -1
 }
+
+// GetEqualLastIndexOf fromから逆順にvalと最初に一致したindexを返す.
+// 見つからなかったときは-1を返す
+func GetEqualLastIndexOf(from []interface{}, val interface{}) int {
+	for a := range from {
+		i := len(from) - a - 1
+		f := from[i]
+		res := reflect.DeepEqual(f, val)
+		if res {
+			return i
+		}
+	}
+	return -1
+}
